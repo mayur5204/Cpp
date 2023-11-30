fad6e28df79d06f86d6bf8c4d11cac82242906b6
mayur5204 <143496615+mayur5204@users.noreply.github.com>
2023-11-30 09:02:11 +0530

    new project shape added

---
+++ JAVA/Main.java
@@ -0,0 +1,57 @@
+// Design a base class shape with two double type values and member functions 
+// to input the data and compute_area() for calculating area of shape.
+//  Derive two classes: triangle and rectangle. Make compute_area() as
+//  abstract function and redefine this function in the derived class to suit their requirements. 
+// Write a program that accepts dimensions of triangle/rectangle and display calculated area.
+//  Implement dynamic binding for given case study.
+
+package JAVA;
+import java.util.Scanner;
+
+
+
+abstract class Shape
+{
+
+    protected double dim1,dim2;
+
+    abstract void input_data();
+    abstract double compute_area();
+
+}
+
+class Triangle extends Shape
+{
+
+    @Override
+    void input_data()
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the base of the triangle : ");
+        dim1=sc.nextDouble();
+        System.out.println("Enter the height of the triangle : ");
+        dim2=sc.nextDouble();
+        sc.close();
+    }
+
+    @Override
+    protected double compute_area()
+    {
+        return (dim1*dim2)/2;
+    
+    }
+   
+}
+
+public class Main{
+      public static void main(String args[])
+    {
+        Shape shape;
+        shape =new Triangle();
+        shape.input_data();
+        shape.compute_area();
+    }
+}
+   
+
+
